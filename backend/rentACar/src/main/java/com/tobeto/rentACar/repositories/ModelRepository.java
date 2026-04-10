fbe953bc3b125805a0ca5ebb91dcbe1ee9de0185
khangtd <khang08012k4@gmail.com>
2026-04-10 11:12:02 +0700

    chore: remove demo data initialization scripts and related files; update README and environment configurations to reflect changes in demo setup

--- backend/rentACar/src/main/java/com/tobeto/rentACar/repositories/ModelRepository.java
+++ backend/rentACar/src/main/java/com/tobeto/rentACar/repositories/ModelRepository.java
@@ -3,11 +3,7 @@ package com.tobeto.rentACar.repositories;
 import com.tobeto.rentACar.entities.concretes.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ModelRepository extends JpaRepository<Model, Integer> {
 
     boolean existsByName(String name);
-
-    Optional<Model> findByNameAndBrand_Id(String name, Integer brandId);
 }
